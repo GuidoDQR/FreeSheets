9edfb301efe50a506314e78ad88a0dffb6c1a155
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2023-11-10 13:05:38 -0300

    new atributes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let groupCell = document.getElementById("group-cell");
-let groupRow = document.getElementById("group-row");
+let groupRow = document.getElementsByClassName("group-row");
 let contextMenu = document.getElementById("contextMenu");
 
 let char;
@@ -8,26 +8,37 @@ for(let i=0; i<26; i++){
     char = String.fromCharCode(65+i);
     newDiv.className = "cell";
     newDiv.innerText = char;
-    groupRow.appendChild(newDiv);
+    groupRow[0].appendChild(newDiv);
 }
 
 for(let i=1; i<10; i++){
-    const newRow = document.createElement("div");
-    newRow.id = "group-row";
-    groupCell.appendChild(newRow);
+    const rowAttribute = document.createAttribute("row");
+    rowAttribute.value = i;
+
+    const newGroupRow = document.createElement("div");
+    newGroupRow.className = "group-row";
+    groupCell.appendChild(newGroupRow);
+
     const newDiv = document.createElement("div");
     newDiv.className = "row";
+    newDiv.setAttributeNode(rowAttribute);
     newDiv.innerText = i.toString();
-    newRow.appendChild(newDiv);
-    for(let j=0; j<26; j++){
+    newGroupRow.appendChild(newDiv);
+
+    for(let j=1; j<=26; j++){
+        
         const newDiv = document.createElement("div");
         newDiv.className = "cell";
-        newRow.appendChild(newDiv);
+        newDiv.setAttributeNode(rowAttribute.cloneNode());
+        
+        newGroupRow.appendChild(newDiv);
     }
 }
 
-// groupCell.addEventListener("mouseover", setMenu);
-// groupCell.addEventListener("mouseout", exitMenu);
+let cell = document.getElementsByClassName("cell");
+// cell.
+groupCell.addEventListener("mouseover", setMenu);
+groupCell.addEventListener("mouseout", exitMenu);
 
 
 
@@ -45,7 +56,7 @@ contextMenu.oncontextmenu = rightClick;
 
 console.log(document.oncontextmenu);
 console.log(groupCell.oncontextmenu);
-// document.oncontextmenu = rightClick;
+
 function setMenu(){
     console.log("entre");
 }
